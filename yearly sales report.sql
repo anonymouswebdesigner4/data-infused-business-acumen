861d1ca586724f2631de08fcd75abc145718d4dd
Amuktha <71584504+anonymouswebdesigner4@users.noreply.github.com>
2023-08-24 16:51:31 +0530

    Add files via upload

---
+++ yearly sales report.sql
@@ -0,0 +1,9 @@
+#Yearly Sales Report
+select get_fiscal_year(date) as fiscal_year,round((gross_price*sold_quantity),2) as yearly_sales from fact_sales_monthly s
+join fact_gross_price g
+on 
+g.product_code=s.product_code and
+g.fiscal_year=get_fiscal_year(s.date)
+where customer_code=90002002
+group by get_fiscal_year(s.date)
+order by s.date asc;
